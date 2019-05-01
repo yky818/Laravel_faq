43530fb861c1116a0d5d8b5184e630e025f520b0
Developer <yky5@njit.edu>
2019-05-01 12:54:11 +0530

    Added Answer migration, model and Answer Table seeder with factory. - Added relation between Answer with Question and user tables in models. - Added Seed data in Answer table with 6 Answers per Question.

--- app/Answer.php
+++ app/Answer.php
@@ -4,13 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Question extends Model
+class Answer extends Model
 {
     public function user() {
         return $this->belongsTo('App\User');
     }
-
-    public function answers() {
-        return $this->hasMany('App\Answer');
+    public function question() {
+        return $this->belongsTo('App\Question');
     }
 }
--- app/User.php
+++ app/User.php
@@ -44,4 +44,8 @@ class User extends Authenticatable
     public function questions() {
         return $this->hasMany('App\Question');
     }
+
+     public function answers() {
+        return $this->hasMany('App\Answer');
+    }
 }
