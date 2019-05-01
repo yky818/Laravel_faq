ed68ae21c23ac776d67d2f54933508a12352ce81
Developer <yky5@njit.edu>
2019-05-01 12:26:39 +0530

    Added Question migration, model and Question Table seeder with factory.
    - Added relation between User and Question tables in models.
    - Added Seed data in Question table with 16 questions per user.

--- app/User.php
+++ app/User.php
@@ -40,4 +40,8 @@ class User extends Authenticatable
     public function profile() {
         return $this->hasOne('App\Profiles');
     }
+
+    public function questions() {
+        return $this->hasMany('App\Question');
+    }
 }
