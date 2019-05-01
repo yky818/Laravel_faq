ebd9688266e09f64707ef69e628aa606a9eb6270
Developer <yky5@njit.edu>
2019-05-01 11:02:30 +0530

    Added Unit test to insert user with profile.

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testSave()
+    {
+        $user = factory(\App\User::class)->make();
+        $user->save();
+        $profile = factory(\App\Profiles::class)->make();
+        $profile->user()->associate($user);
+        $this->assertTrue($profile->save());
+    }
+}
